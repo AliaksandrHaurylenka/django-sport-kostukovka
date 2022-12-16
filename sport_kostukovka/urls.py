114884b1d5c84bb693994bc06868ccc2250b741e
Гавриленко Александр Дмитриевич <a.gavrilenko@stekloluks.local>
2022-12-16 17:54:25 +0300

    class

--- sport_kostukovka/urls.py
+++ sport_kostukovka/urls.py
@@ -5,10 +5,10 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('sport_sections/', sport_sections, name='sport_sections'),
-    path('news/', news, name='news'),
+    path('news/', SportKostukovkaNews.as_view(), name='news'),
     path('addpage/', addpage, name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('news/<slug:post_slug>/', show_news, name='post'),
-    path('category/<int:cat_id>/', show_category, name='category'),
+    path('category/<slug:cat_slug>/', SportKostukovkaCategory.as_view(), name='category'),
 ]
--- sport_kostukovka/views.py
+++ sport_kostukovka/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -17,19 +18,33 @@ def index(request):
     return render(request, 'sport_kostukovka/index.html', {'news': news, 'menu': menu, 'title': 'Спорт-Костюковка'})
 
 
-def news(request):
-    posts = News.objects.all()
-    cats = Category.objects.all()
+class SportKostukovkaNews(ListView):
+    model = News
+    template_name = 'sport_kostukovka/news.html'
+    context_object_name = 'posts'
 
-    context = {
-        'news': posts,
-        'menu': menu,
-        'cats': cats,
-        'cat_selected': 0,
-        'title': 'Спортивные события'
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная'
+        context['cat_selected'] = 0
+        return context
 
-    return render(request, 'sport_kostukovka/news.html', context=context)
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+# def news(request):
+#     posts = News.objects.all()
+#     cats = Category.objects.all()
+#
+#     context = {
+#         'news': posts,
+#         'menu': menu,
+#         'cats': cats,
+#         'cat_selected': 0,
+#         'title': 'Спортивные события'
+#     }
+#
+#     return render(request, 'sport_kostukovka/news.html', context=context)
 
 
 def addpage(request):
@@ -80,19 +95,34 @@ def show_news(request, post_slug):
     return render(request, 'sport_kostukovka/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = News.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+class SportKostukovkaCategory(ListView):
+    model = News
+    template_name = 'sport_kostukovka/news.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return News.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'news': posts,
-        'menu': menu,
-        'cats': cats,
-        'title': 'Спортивные события',
-        'cat_selected': cat_id,
-    }
-
-    return render(request, 'sport_kostukovka/news.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+# def show_category(request, cat_id):
+#     posts = News.objects.filter(cat_id=cat_id)
+#     cats = Category.objects.all()
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'news': posts,
+#         'menu': menu,
+#         'cats': cats,
+#         'title': 'Спортивные события',
+#         'cat_selected': cat_id,
+#     }
+#
+#     return render(request, 'sport_kostukovka/news.html', context=context)
